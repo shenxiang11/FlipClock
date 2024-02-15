ced78487326ba934f852676b71e0b7d306eaf74a
Shen <863461783@qq.com>
2024-02-15 09:21:49 +0800

    Initial Commit

---
+++ FlipClockAnimation/FlipClockAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClockAnimationApp.swift
+//  FlipClockAnimation
+//
+//  Created by 香饽饽zizizi on 2024/2/15.
+//
+
+import SwiftUI
+
+@main
+struct FlipClockAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
